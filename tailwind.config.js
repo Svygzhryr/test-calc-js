60bf70084841c3c4fadcd9229bfb554e1d6c0e8c
Svygzhryr <pamixy@gmail.com>
2025-04-01 07:58:52 +0300

    fix: tailwind theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        main: {
-          0: "#bdbdff",
-          50: "#8888f7",
-          100: "#6868ed",
-          200: "#4e4ecc",
-          300: "#3e3eab",
-          400: "#363685",
-          500: "#2f2f61",
-          600: "#26264a",
-          700: "#1f1f38",
-          800: "#1b1b31",
-          900: "#11111a",
-          1000: "#0a0a0f",
-          text: "#e4e4f5",
-          error: "#F82174",
-        },
-      },
-    },
-  },
   plugins: [],
 };
